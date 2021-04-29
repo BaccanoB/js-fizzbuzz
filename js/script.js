c43cac3a8bc6d7ece6dddb1d940a2f5fd605452b
BaccanoB <beatrice.baccano@yahoo.it>
2021-04-29 15:47:06 +0200

    inseriti fizz e buzz

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Stampare numeri da 1 a 100
 var numeri = []; 
-var fizz = "fizz";
-var buzz;
-var fizzabuzz;
+var fizz = "Fizz";
+var buzz = "Buzz";
+var fizzbuzz = "FizzBuzz";
 
 for (var n=1; n<=100; n++){
     numeri.push(n);
@@ -10,10 +10,15 @@ for (var n=1; n<=100; n++){
 
 for(var i = 0; i < numeri.length; i++) {
     console.log(numeri[i]);
-    if (numeri[i] %3 == 0) {
+    if (numeri[i] %3 == 0){
         numeri[i] = fizz;
         console.log(numeri[i]);
+    }else if (numeri[i] %5 == 0){
+        numeri[i] = buzz;
+        console.log(numeri[i]);
+    }else if  ((numeri[i] %3 == 0) && (numeri[i] %5 == 0)) {
+        numeri[i] = fizzbuzz;
+        console.log(numeri[i]); 
     }
-}
+}  
 
-// per i multipli di 3 stampare fizz, per i multipli di 5 stmpare buzz, per im ultipli di entrambi stampare fizzbuzz
