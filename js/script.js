2652f3c801c2cae028ad1356821f6e7864c2da02
BaccanoB <beatrice.baccano@yahoo.it>
2021-04-29 18:05:56 +0200

    semplificato codice

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
 // Stampare numeri da 1 a 100
-var numeri = []; 
-var fizz = "Fizz";
-var buzz = "Buzz";
-var fizzbuzz = "FizzBuzz";
 
-for (var n=1; n<=100; n++){
-    numeri.push(n);
-}
+// var numeri = []; 
 
-for(var i = 0; i < numeri.length; i++) {
-    if ((numeri[i] %3 == 0) && (numeri[i] %5 == 0)) {
-        numeri[i] = fizzbuzz;
-        console.log(numeri[i] = fizzbuzz);
-    }else if (numeri[i] %5 == 0){
-        numeri[i] = buzz;
-        console.log(numeri[i] = buzz);
-    }else if (numeri[i] %3 == 0){
-        numeri[i] = fizz;
-        console.log(numeri[i] = fizz );
-    }else {
-        console.log(numeri[i]);
+// for (var n=1; n<=100; n++){
+//     numeri.push(n);
+// }
+
+// for(var i = 0; i <= 100; i++) {
+//     if ((numeri[i] %3 == 0) && (numeri[i] %5 == 0)) {
+//         numeri[i] = "Fizzbuzz";
+//         console.log(numeri[i]);
+//     }else if (numeri[i] %5 == 0){
+//         numeri[i] = "Buzz";
+//         console.log(numeri[i]);
+//     }else if (numeri[i] %3 == 0){
+//         numeri[i] = "Fizz";
+//         console.log(numeri[i]);
+//     }else {
+//         console.log(numeri[i]);
+//     }
+// }  
+
+for(var i = 0; i <= 100; i++) {
+    if ((i%3 == 0) && (i %5 == 0)) {
+        console.log("Fizzbuzz");
+    }else if (i%5 == 0){
+        console.log("Buzz");
+    }else if (i%3 == 0){
+        console.log("Fizz");
+    } else {
+        console.log(i);
     }
-}  
+}
 
 
